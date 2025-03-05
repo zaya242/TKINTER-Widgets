392da3a581e2c3d809ee0a7b469fe791f4e73ba6
zaya242 <isiomajadesimi@gmail.com>
2025-03-05 17:32:14 +0100

    activity1

---
+++ activity1.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+window=Tk()
+window.title("TKinter image")
+
+upload=Image.open("img1.jpg")
+image=ImageTk.PhotoImage(upload)
+
+label=Label(master=window,image=image)
+label1=Label(master=window,text="Thia is how to add an image")
+
+label.pack()
+label1.pack()
+
+window.mainloop()
